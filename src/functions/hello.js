97af88f9c4aa8133ef325caac91d68e505da65e7
William <william@jansen.net>
2025-09-08 13:37:18 +0200

    msal implementation

--- src/functions/hello.js
+++ src/functions/hello.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const jwksClient = require('jwks-rsa');
 
 const tenantId = 'a18232f7-c6f8-48da-b8e1-838c7fac8ab1'; // Replace with your Azure AD tenant ID
-const audience = 'b82efeaa-410b-4d9a-9bc1-64a1a3f71ec9'; // Replace with your Azure AD app registration client ID
+const audience = 'b82efeaa-410b-4d9a-9bc1-64a1a3f71ec9'; // Replace with your Azure AD app registration client ID (function app)
 
 const client = jwksClient({
   jwksUri: `https://login.microsoftonline.com/${tenantId}/discovery/v2.0/keys`
@@ -25,22 +25,18 @@ app.http('hello', {
     authLevel: 'function',
     handler: async (request, context) => {
         context.log(`Http function processed request for url "${request.url}"`);
-
-        // const name = request.query.get('name') || await request.text() || 'world';
-        // Log all headers for debugging
         
         const headers = Object.fromEntries(request.headers.entries());
-        context.log('Authorization:', headers['authorization']);
-
         const authHeader = headers['authorization'];
+        context.log('Authorization header: ', authHeader);
         if (!authHeader || !authHeader.startsWith('Bearer ')) {
             //return { status: 401, body: 'Missing or invalid Authorization header' };
             return { body: "Missing or invalid Authorization header" };
         }
 
         const token = authHeader.substring(7);
-        context.log('token:', token);
-        
+        context.log('token: ', token);
+
         try {
             const decoded = await new Promise((resolve, reject) => {
                 jwt.verify(token, getKey, {audience: audience, issuer: `https://login.microsoftonline.com/${tenantId}/v2.0`},
@@ -51,7 +47,7 @@ app.http('hello', {
             });
 
             // Log all claims for debugging
-            context.log('JWT claims:', JSON.stringify(decoded, null, 2));
+            context.log('JWT claims: ', JSON.stringify(decoded, null, 2));
 
             // Token is valid, you can access claims in 'decoded'
             return { body: `Hello from func API, ${decoded.name || decoded.preferred_username || 'user'}!` };
